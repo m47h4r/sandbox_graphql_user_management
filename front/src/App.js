08cf10c7ffa2a44173ab6e9ad3ac84a8dc16e0f0
m47h4r <m47h4r@gmail.com>
2020-09-21 19:32:06 +0330

    first data fetch from apollo

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,22 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 
+const GET_ALL_USERS = gql`
+  {
+    getAllUsers {
+      id
+      firstName
+      lastName
+      email
+      password
+    }
+  }
+`;
+
 function App() {
-  return <p>Here will be the app</p>;
+  const { data, loading, error } = useQuery(GET_ALL_USERS);
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3001/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
